f3d3a298278586a5f86503334a98202554ad946b
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2021-12-16 18:37:21 -0800

    make better

--- connection/dbConnection.js
+++ connection/dbConnection.js
@@ -3,29 +3,29 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; // heroku addon
+// const proConfig = process.env.DATABASE_URL; // heroku addon
 
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-});
+// const pool = new Pool({
+//   connectionString:
+//     process.env.NODE_ENV === "production" ? proConfig : devConfig,
+// });
 
 // === OTHER WAY TO CONFIGURE === //
 
-// const devConfig = {
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-//   host: process.env.PG_HOST,
-//   port: process.env.PG_PORT,
-//   database: process.env.PG_DATABASE,
-// };
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+};
 
-// const proConfig = { connectionString: process.env.DATABASE_URL };
+const proConfig = { connectionString: process.env.DATABASE_URL };
 
-// const pool = new Pool(
-//   process.env.NODE_ENV === "production" ? proConfig : devConfig
-// );
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 export default pool;
